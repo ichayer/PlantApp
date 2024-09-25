360eade6c31153303d62b751780923857fda2a77
ichayer <ichayer@itba.edu.ar>
2024-09-25 11:35:43 -0300

    Start developing HTTP API GW

--- terraform/api_gw/main.tf
+++ terraform/api_gw/main.tf
@@ -1,70 +1,59 @@
-resource "aws_api_gateway_rest_api" "planty_api" {
-  name        = "planty-API"
-  description = "API Gateway"
+resource "aws_apigatewayv2_api" "planty_http_api" {
+  name          = "planty-http-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_headers = ["*"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+    max_age       = 86400
+  }
 }
 
-resource "aws_api_gateway_resource" "plants" {
-  rest_api_id = aws_api_gateway_rest_api.planty_api.id
-  parent_id   = aws_api_gateway_rest_api.planty_api.root_resource_id
-  path_part   = "plants"
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.planty_http_api.id
+  name        = "$default"
+  auto_deploy = true
 }
 
-resource "aws_api_gateway_method" "plants_method" {
-  rest_api_id   = aws_api_gateway_rest_api.planty_api.id
-  resource_id   = aws_api_gateway_resource.plants.id
-  http_method   = "ANY"
-  authorization = "NONE"
+resource "aws_apigatewayv2_route" "plants_route" {
+  api_id    = aws_apigatewayv2_api.planty_http_api.id
+  route_key = "ANY /plants"
+  target    = "integrations/${aws_apigatewayv2_integration.plants_integration.id}"
 }
 
-resource "aws_api_gateway_integration" "plants_integration" {
-  rest_api_id = aws_api_gateway_rest_api.planty_api.id
-  resource_id = aws_api_gateway_resource.plants.id
-  http_method = aws_api_gateway_method.plants_method.http_method
-  integration_http_method = "ANY"
-  type                    = "AWS_PROXY"
-  uri                     = var.plants_invoke_arn
+resource "aws_apigatewayv2_route" "plant_by_id_route" {
+  api_id    = aws_apigatewayv2_api.planty_http_api.id
+  route_key = "ANY /plants/{plantId}"
+  target    = "integrations/${aws_apigatewayv2_integration.plant_by_id_integration.id}"
 }
 
-resource "aws_api_gateway_resource" "plant_by_id" {
-  rest_api_id = aws_api_gateway_rest_api.planty_api.id
-  parent_id   = aws_api_gateway_resource.plants.id
-  path_part   = "{plantId}"
+resource "aws_apigatewayv2_route" "plant_waterings_route" {
+  api_id    = aws_apigatewayv2_api.planty_http_api.id
+  route_key = "ANY /plants/{plantId}/waterings"
+  target    = "integrations/${aws_apigatewayv2_integration.plant_waterings_integration.id}"
 }
 
-resource "aws_api_gateway_method" "plant_by_id_method" {
-  rest_api_id   = aws_api_gateway_rest_api.planty_api.id
-  resource_id   = aws_api_gateway_resource.plant_by_id.id
-  http_method   = "ANY"
-  authorization = "NONE"
+resource "aws_apigatewayv2_integration" "plants_integration" {
+  api_id                  = aws_apigatewayv2_api.planty_http_api.id
+  integration_type         = "AWS_PROXY"
+  integration_uri          = var.plants_invoke_arn
+  integration_method       = "POST"
+  payload_format_version   = "2.0"
 }
 
-resource "aws_api_gateway_integration" "plant_by_id_integration" {
-  rest_api_id = aws_api_gateway_rest_api.planty_api.id
-  resource_id = aws_api_gateway_resource.plant_by_id.id
-  http_method = aws_api_gateway_method.plant_by_id_method.http_method
-  integration_http_method = "ANY"
-  type                    = "AWS_PROXY"
-  uri                     = var.plantsById_invoke_arn
+resource "aws_apigatewayv2_integration" "plant_by_id_integration" {
+  api_id                  = aws_apigatewayv2_api.planty_http_api.id
+  integration_type         = "AWS_PROXY"
+  integration_uri          = var.plantsById_invoke_arn
+  integration_method       = "POST"
+  payload_format_version   = "2.0"
 }
 
-resource "aws_api_gateway_resource" "plant_waterings" {
-  rest_api_id = aws_api_gateway_rest_api.planty_api.id
-  parent_id   = aws_api_gateway_resource.plant_by_id.id
-  path_part   = "waterings"
-}
-
-resource "aws_api_gateway_method" "plant_waterings_method" {
-  rest_api_id   = aws_api_gateway_rest_api.planty_api.id
-  resource_id   = aws_api_gateway_resource.plant_waterings.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "plant_waterings_integration" {
-  rest_api_id = aws_api_gateway_rest_api.planty_api.id
-  resource_id = aws_api_gateway_resource.plant_waterings.id
-  http_method = aws_api_gateway_method.plant_waterings_method.http_method
-  integration_http_method = "ANY"
-  type                    = "AWS_PROXY"
-  uri                     = var.plantsByIdWaterings_invoke_arn
+resource "aws_apigatewayv2_integration" "plant_waterings_integration" {
+  api_id                  = aws_apigatewayv2_api.planty_http_api.id
+  integration_type         = "AWS_PROXY"
+  integration_uri          = var.plantsByIdWaterings_invoke_arn
+  integration_method       = "POST"
+  payload_format_version   = "2.0"
 }
