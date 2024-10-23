830d6bf3973192d18ccb733c293b503d375a7a04
tataabancens <tataabancens@gmail.com>
2024-10-23 12:40:36 -0300

    Removing build_frontend.sh, script directly in s3 file

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -40,7 +40,15 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
 resource "null_resource" "build_frontend" {
   provisioner "local-exec" {
     working_dir = "${path.root}/../frontend"
-    command     = "./build_frontend.sh"
+        command     = <<EOT
+      echo "export const url = '$API_ENDPOINT'" > ./src/lib/api.ts;
+      echo "export const userPoolId = '$USER_POOL_ID'" > ./src/lib/cognito.ts;
+      echo "export const clientId = '$CLIENT_ID'" >> ./src/lib/cognito.ts;
+      echo "export const region = '$REGION'" >> ./src/lib/cognito.ts;
+
+      npm ci;
+      npm run build;
+    EOT
     environment = {
       "API_ENDPOINT" = var.api_endpoint
       "USER_POOL_ID" = var.cognito_user_pool_id
@@ -48,6 +56,9 @@ resource "null_resource" "build_frontend" {
       "REGION"       = var.region
     }
     interpreter = ["bash", "-c"]
+  }
+    triggers = {
+    "run_always" = plantimestamp()
   }
 }
 
